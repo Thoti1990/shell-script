b7b2727edc3d0cd411e09ed9a54a7ab6f3b52310
Thoti1990 <thotinagaraju437@gmail.com>
2024-11-11 18:39:46 +0530

    2nd commited

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,6 +4,6 @@ PERSION1=Raju
 PERSION2=Senu
 
 echo "$PERSION1:: Hello $PERSION2, How are you"
-echo "S$PERSION2:: Hi $PERSION1, I am good how about you"
+echo "$PERSION2:: Hi $PERSION1, I am good how about you"
 echo "$PERSION1:: what are the palans $PERSION1 "
-echo "S$PERSION2:: $PERSION1 i went for village"
+echo "$PERSION2:: $PERSION1 i went for village"
