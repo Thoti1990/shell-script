488d7e14110462c34d9a06a80f80b14fbe1f806f
Thoti1990 <thotinagaraju437@gmail.com>
2024-11-11 18:44:41 +0530

    args commited

---
+++ 03-args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSION1=$1
+PERSION2=$2
+
+echo "$PERSION1:: Hello $PERSION2, How are you"
+echo "$PERSION2:: Hi $PERSION1, I am good how about you"
+echo "$PERSION1:: what are the palans $PERSION1 "
+echo "$PERSION2:: $PERSION1 i went for village"
