dde4ce949e866b339f8d909ddd1f02a8c9f04909
Thoti1990 <thotinagaraju437@gmail.com>
2024-11-11 18:38:33 +0530

    2nd commited

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Raju:: Hello Senu, How are you"
-echo "Senu:: Hi Raju, I am good how about you"
-echo "Raju:: what are the palans Raju "
-echo "Senu:: Raju i went for village"
+PERSION1=Raju
+PERSION2=Senu
+
+echo "$PERSION1:: Hello $PERSION2, How are you"
+echo "S$PERSION2:: Hi $PERSION1, I am good how about you"
+echo "$PERSION1:: what are the palans $PERSION1 "
+echo "S$PERSION2:: $PERSION1 i went for village"
