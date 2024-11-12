966bbbe9c14e85ceb53c1989fe3afcb81693b966
Thoti1990 <thotinagaraju437@gmail.com>
2024-11-12 17:44:22 +0530

    log file

--- 10-log.sh
+++ 10-log.sh
@@ -4,13 +4,20 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+echo "Script started exequting at $Y $TIMESTAMP $N" &>> $LOGFILE
+
 VALIDATION(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2.. FAILED"
+        echo "ERROR:: $2.. $R FAILED $N"
         exit 1
     else
-        echo "$2.. SUCCESS"
+        echo "$2..  $G SUCCESS $N"
     fi
 }
 
@@ -18,7 +25,7 @@ ID=$(id -u)
 
 if [ $ID -ne 0 ]
 then 
-    echo "ERROR:: Please run the script with root user"
+    echo " $R ERROR:: Please run the script with root user $N"
     exit 1
 else
     echo "your are in root user"
